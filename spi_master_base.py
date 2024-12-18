f404a5a6ce25e2770b59da5d667f1bfe158a42f7
pascalguttmann <github@pascal-guttmann.de>
2024-12-18 19:26:33 +0100

    edit class SpiMasterBase typehint return types of abstractmethods

--- spi_master_base.py
+++ spi_master_base.py
@@ -4,15 +4,17 @@ from typing import TypeVar
 
 class SpiMasterBase(ABC):
     @abstractmethod
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         """Initialize the SPI bus master to allow data transfer to slave devices"""
 
     @abstractmethod
-    def transfer(self, cs: int, buf: bytearray):
-        """Transfer content of 'buf' via SPI bus with chip select 'cs' enabled
+    def transfer(self, cs: int, buf: bytearray) -> bytearray:
+        """Transfer content of 'buf' via SPI bus with chip select 'cs' enabled and
+        receive bytes
 
         :param cs: id of chip select used for SPI transfer
-        :param buf: bytearray containing bytes to be sent, will be filled with received bytes
+        :param buf: bytearray containing bytes to be sent
+        :return: bytearray containing bytes received
         """
 
 
