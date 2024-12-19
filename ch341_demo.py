c39f9a57537b3ff2a0595aacadd87c76ecec01d2
pascalguttmann <github@pascal-guttmann.de>
2024-12-19 19:06:12 +0100

    add ch341 demo argument parsing

--- ch341_demo.py
+++ ch341_demo.py
@@ -1,7 +1,19 @@
+import sys
+
 from ch341.ch341 import CH341
 
-ch341 = CH341()
-tx_buf = bytearray([0x55])
-rx_buf = ch341.transfer(cs=0, buf=tx_buf)
 
-print(f"TX: {tx_buf}\nRX: {rx_buf}")
+def hex_to_bytearray(hex_string):
+    bytes_object = bytes.fromhex(hex_string)
+    bytearray_object = bytearray(bytes_object)
+    return bytearray_object
+
+
+hex_string = sys.argv[1]
+tx_arg = hex_to_bytearray(hex_string)
+print(f"bytearray: {tx_arg.hex()}")
+ch341 = CH341()
+tx_buf = tx_arg
+rx_buf = ch341.transfer(cs=0, buf=tx_buf)
+print(f"TX: 0x{tx_buf.hex()}")
+print(f"RX: 0x{rx_buf.hex()}")
