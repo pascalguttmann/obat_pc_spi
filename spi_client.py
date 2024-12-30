1a8f1caeea87dec2694838630da957870155fff7
pascalguttmann <github@pascal-guttmann.de>
2024-12-30 22:23:34 +0100

    add spi client __main__ guards

--- spi_client.py
+++ spi_client.py
@@ -1,52 +1,58 @@
 """Spi client for debugging and demonstration. Client will create an instance
 of the spi driver server for a given hardware spi interface and connect to it.
 Arbitrary data can be sent and received using the client."""
-(
-    client_to_server_pipe,
-    server_to_client_pipe,
-    server_read,
-    server_write,
-    b64_server_ipc as ipc,
-    pack_server_response,
-    unpack_server_command,
-)
 
-from spi_driver_ipc import (
-    b64_client_ipc as ipc,
-    client_to_server_pipe,
-    server_to_client_pipe,
-    client_read_pipe_end,
-    client_write_pipe_end,
-    pack_server_command,
-    unpack_server_response,
+if __name__ == "__main__":
 
-)
-from spi_server import SpiServer
-from ch341.ch341 import CH341
+    import sys
+    import os
 
-with SpiServer(CH341()) as spi_server:
-    with client_write_pipe_end as wr:
-        with client_read_pipe_end as rd:
-    
+    sys.path.append(
+        os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "python_xp_named_pipe/",
+        )
+    )
 
-            try:
-                while True:
-                    user_input = input("Enter something (or 'quit' to exit): ")
-                    if user_input.lower() == "quit":
-                        print("Exiting program.")
-                        break
-                    # Process the user input here
-                    print("You entered:", user_input)
-            except KeyboardInterrupt:
-                print("SIGINT: Exiting program.")
+    from spi_driver_ipc import (
+        b64_client_ipc as ipc,
+        client_to_server_pipe,
+        server_to_client_pipe,
+        client_read_pipe_end,
+        client_write_pipe_end,
+        pack_server_command,
+        unpack_server_response,
+    )
+    from spi_server import SpiServer
+    from ch341.ch341 import CH341
 
-            if __name__ == "__main__":
+    def hex_string_to_bytes(hex_string):
+        if hex_string.startswith("0x"):
+            hex_string = hex_string[2:]
 
-                import argparse
+        if len(hex_string) % 2 != 0:
+            hex_string = "0" + hex_string
 
-                parser = argparse.ArgumentParser()
-                parser.add_argument(
-                    "spi_master", help="The device used as an spi master. E.g. ch34x_pis1"
-                )
+        return bytes.fromhex(hex_string)
 
-                spi_server = SpiServer(spi_master=sys.argv[1])
+    with SpiServer(CH341()) as spi_server:
+        with client_write_pipe_end:
+            with client_read_pipe_end:
+
+                cs = 0
+
+                try:
+                    while True:
+                        user_input = input("Enter hexstring or 'exit': ")
+                        if user_input.lower() == "exit":
+                            print("Exiting program.")
+                            break
+                        tx_bytes = hex_string_to_bytes(user_input)
+
+                        ipc.write(pack_server_command(cs, tx_bytes))
+                        rx_bytes = unpack_server_response(ipc.read())
+
+                        print(f"TX: {tx_bytes.hex()}, RX: {rx_bytes.hex()}")
+
+                except KeyboardInterrupt:
+                    print("SIGINT: Exiting program.")
