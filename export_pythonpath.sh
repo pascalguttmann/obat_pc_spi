bb6a313520ae5fe52eedec21fdfe28cbfc7cbfaa
pascalguttmann <github@pascal-guttmann.de>
2025-01-05 00:32:22 +0100

    fix export_pythonpath.sh to export path to spi_operation module

--- export_pythonpath.sh
+++ export_pythonpath.sh
@@ -6,3 +6,4 @@ export PYTHONPATH=$SCRIPT_DIR
 export PYTHONPATH+=:$SCRIPT_DIR/python_xp_named_pipe
 export PYTHONPATH+=:$SCRIPT_DIR/spi_elements
 export PYTHONPATH+=:$SCRIPT_DIR/spi_master
+export PYTHONPATH+=:$SCRIPT_DIR/spi_operation
