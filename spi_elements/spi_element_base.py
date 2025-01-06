5acfe083e47a1b051d268ee5926d217dfb4e639d
pascalguttmann <github@pascal-guttmann.de>
2025-01-06 08:04:46 +0100

    remove spi_element_base name and spi_child_elements for application specific implementation

--- spi_elements/spi_element_base.py
+++ spi_elements/spi_element_base.py
@@ -18,51 +18,12 @@ class SingleTransferOperationRequest:
 class SpiElementBase(ABC):
     def __init__(
         self,
-        name: Optional[str],
-        spi_element_childs: Optional[List[SpiElementBase]],
         *args,
         **kwargs,
     ) -> None:
         """Initialize the SPI bus master object"""
         _, _ = args, kwargs
-        self._set_spi_element_childs(spi_element_childs)
         self._operation_request = Queue()
-        self._set_name(name)
-
-    def _set_spi_element_childs(
-        self, spi_element_childs: Optional[List[SpiElementBase]]
-    ) -> None:
-        self._spi_element_childs = spi_element_childs
-
-    def _set_name(self, name: Optional[str]) -> None:
-        if name and not self._name_in_sub_elements(name):
-            self._name = name
-        else:
-            raise ValueError(
-                f"Name {name} of SpiElement is not unique among child SpiElement."
-            )
-
-    def _name_in_sub_elements(self, name: str) -> bool:
-        if not self._spi_element_childs:
-            return False
-        else:
-            return any(
-                name == sub._name or sub._name_in_sub_elements(name)
-                for sub in self._spi_element_childs
-            )
-
-    def get_spi_element_by_name(self, name: str) -> SpiElementBase:
-        if name == self._name:
-            return self
-        else:
-            if self._spi_element_childs:
-                for sub in self._spi_element_childs:
-                    try:
-                        return sub.get_spi_element_by_name(name)
-                    except ValueError:
-                        continue
-
-        raise ValueError(f"Name: '{name}' not found for SpiElement: {self}")
 
     def pop_unprocessed_operation_request(self) -> SingleTransferOperationRequest:
         """Pop the next operation request, that should be written to the
