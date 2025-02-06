31130b0d556d376d6b15cb520ae937b5ff5c2ff3
pascalguttmann <github@pascal-guttmann.de>
2025-02-06 12:33:52 +0100

    fix ad5672 init
    init must not reset the ad5672r for compatibility with other spi devices
    in a daisychain or in other ways disable the daisychain mode.

--- device_implementation/dac/ad5672/functional_operations.py
+++ device_implementation/dac/ad5672/functional_operations.py
@@ -14,8 +14,79 @@ class Initialize(SequenceTransferOperation):
         """Initialize the dac for operation in daisychain with 0V to 5V output range."""
         ops = []
 
-        ops.append(op.SoftwareReset())
-        ops.append(op.SetDcEnMode())
+        # DAISYCHAIN mode
+        # ===============
+        #
+        # For daisychain operation with multiple devices the device MUST NOT be
+        # reset or the daisychain mode disabled. In order to not put the entire
+        # daisychain in a disfunctional state. (The chain can only be brought back
+        # into functional state, by a write sequence of unequal bitlength, which is
+        # by design not supported by the obat software at this point (06. Feb 2025).)
+        #
+        # Reason
+        # ------
+        #
+        # Ad5672R features daisychain mode == enabled or daisychain mode == disabled
+        #
+        # ### daisychain mode == enabled
+        #
+        # The chip works, as one would expect, like a shift register.
+        #
+        # ### daisychain mode == disabled
+        #
+        # The chip requires exact writes of
+        # 24-bit in order to execute the desired command. Contrary to behavior, that
+        # would be easy to implement with equal length writes, the chip in this mode
+        # clocks in the first 24 bits transmitted and ignores following bits.
+        # I.e. a write sequence preceeded by zeros for other chips behind the
+        # ad5672r in mode daisychain disabled prevents a following 0x800001
+        # (command to set daisychain mode to enabled) to be executed by the
+        # ad5672r, because the leading zeros filled the 24 bit expected by the
+        # ad5672r.
+        #
+        # This violates the mental model of the ad5672r in daisychain mode
+        # disabled as a shift register, that has a disable output (i.e. bits
+        # which would be shifted out are just shifted into "nothingness").
+        #
+        # Workaround
+        # ----------
+        #
+        # Before using equal length transfers use a write sequence, _specialized_ to
+        # the daisychain configuration connected to set all the ad5672r chips to
+        # daisychain mode enabled.
+        # After that, continue normally with equal length transfers, without
+        # setting any of the ad5672r to daisychain mode disabled.
+        # Possible ways to set daisychain mode to disabled:
+        #   - Power on reset
+        #   - Software reset (command 0x601234)
+        #   - Explicitly set daisychain mode to disabled (command 0x800000)
+
+        # LDAC Mask register
+        # ==================
+        #
+        # The ldac mask register masks, the functionality of the LDAC pin.
+        #
+        # ldac mask bit == 0
+        # ------------------
+        #
+        # Loading DAC register from input register is possible with:
+        #   - LDAC hardware pin falling *edge*
+        #   - Software command 0x2
+        #   - Software command 0x3
+        #
+        # ldac mask bit == 1
+        # ------------------
+        #
+        # Loading DAC register from input register is possible with:
+        #   - Software command 0x2
+        #   - Software command 0x3
+        #
+        # LDAC hardware pin falling *edges* are ignored.
+        #
+        # When tying LDAC hardware pin permanently low it is possible, that
+        # ad5672r detects this and distinguishes between falling *edges* and
+        # low voltage *levels*.
+
         ops.append(
             op.WriteLoadDacMaskRegister(data=bitarray(reverse_string("11111111")))
         )
