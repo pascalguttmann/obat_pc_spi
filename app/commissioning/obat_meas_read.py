63734b61551b635399a7c9e51be9392539d5a10c
pascalguttmann <github@pascal-guttmann.de>
2025-03-10 22:16:26 +0100

    edit commissioning script for measurment test, add pss output as voltage source

--- app/commissioning/obat_meas_read.py
+++ app/commissioning/obat_meas_read.py
@@ -1,7 +1,11 @@
 if __name__ == "__main__":
     from spi_master.arduino_spi import ArduinoSpi as SpiMaster
     from spi_client_server import SpiChannel, SpiClient, SpiServer
-    from device_implementation.obat import Obat, EnclosurePeltierPolarity
+    from device_implementation.obat import (
+        Obat,
+        EnclosurePeltierPolarity,
+        PssTrackingMode,
+    )
 
     from time import sleep
 
@@ -24,6 +28,19 @@ if __name__ == "__main__":
 
     device.initialize().wait()
 
+    device.get_pss().write_config(
+        tracking_mode=PssTrackingMode.voltage,
+        target_voltage=3.0,
+        target_current=1.0,
+        lower_voltage_limit=+0.0,
+        upper_voltage_limit=+4.0,
+        lower_current_limit=-10.0,
+        upper_current_limit=+10.0,
+    ).wait()
+
+    device.get_pss().output_connect().wait()
+    sleep(0.1)
+
     try:
         while True:
             (voltage, current, temperature) = device.get_meas().read().wait()
@@ -34,6 +51,8 @@ if __name__ == "__main__":
             sleep(0.5)
     except KeyboardInterrupt:
         pass
+
+    device.get_pss().output_disconnect().wait()
 
     client.stop_cyclic_spi_channel_transfer()
 
